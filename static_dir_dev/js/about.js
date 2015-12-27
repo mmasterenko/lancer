1721ba3dbf1903230318dee001f0955399fdaaa5
Maksim Masterenko <mmasterenko@gmail.com>
2015-12-27 12:24:28 +0300

    about page: video complete

---
+++ static_dir_dev/js/about.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('#video-display').prettyEmbed({
+        videoID: 'zsAQZ9IF8Hg',
+        playerControls: false,
+        playerInfo: false,
+        showInfo: false,
+        showControls: true,
+        previewSize: 'hd',
+        //customPreviewImage: 'http://127.0.0.1:8003/static/images/actions/6784385.j88ul0qym1.W958.jpg',
+        showRelated: false,
+        colorScheme: 'light'
+    });
+
+});
+
