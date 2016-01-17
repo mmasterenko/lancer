e9f1f318dd46f5d3f1464e8f7c6312e0c25539ed
Maksim Masterenko <mmasterenko@gmail.com>
2016-01-17 16:17:53 +0300

    models fix

--- lancerApp/admin.py
+++ lancerApp/admin.py
@@ -2,19 +2,23 @@ from django.contrib import admin
 from .models import Car, Service, Spares, TechLiquids
 
 
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'engine', 'transmission')
+
+
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'car', 'price', 'price_cons')
 
 
 class SparesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'car_type', 'price')
+    list_display = ('name', 'car', 'price')
 
 
 class TechLiquidsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'car_type', 'amount', 'price')
+    list_display = ('name', 'car', 'amount', 'price')
 
 
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
 admin.site.register(Service, ServiceAdmin)
 admin.site.register(Spares, SparesAdmin)
 admin.site.register(TechLiquids, TechLiquidsAdmin)
--- lancerApp/models.py
+++ lancerApp/models.py
@@ -3,14 +3,6 @@
 from django.db import models
 
 
-CAR_TYPE = (
-    ('lancer9', 'Lancer 9'),
-    ('lancer10', 'Lancer 10'),
-    ('evo', 'Evolution 6-10'),
-    ('asx', 'Lancer ASX'),
-    ('all', 'Все'),
-)
-
 SERVICE_TYPE = (
     ('oil',       'Замена жидкостей'),
     ('wheel',     'Рулевое управление'),
@@ -29,13 +21,21 @@ class Car(models.Model):
         verbose_name = u'машина'
         unique_together = ('type', 'engine', 'transmission')
 
-    TRANSMISSION_CHOICES = (
+    CAR_TYPE = (
+        ('lancer9', 'Lancer 9'),
+        ('lancer10', 'Lancer 10'),
+        ('evo', 'Evolution 6-10'),
+        ('asx', 'Lancer ASX'),
+        ('all', u'Все модели'),
+    )
+
+    TRANSMISSION_TYPE = (
         ('auto', u'автомат'),
         ('mech', u'механика'),
         ('all',  u'любая'),
     )
 
-    CAPACITY_CHOICES = (
+    ENGINE_CAPACITY = (
         ('1.4', u'1.4'),
         ('1.6', u'1.6'),
         ('1.8', u'1.8'),
@@ -44,23 +44,30 @@ class Car(models.Model):
     )
 
     def __unicode__(self):
-        return '%s %s %s' % (self.get_type_display(), self.get_transmission_display(), self.engine)
+        result = '%s %s %s' % (self.get_type_display(), self.get_transmission_display(), self.get_engine_display())
+        if self.transmission == 'all':
+            result = '%s %s' % (self.get_type_display(), self.get_engine_display())
+        if self.engine == 'all':
+            result = '%s %s' % (self.get_type_display(), self.get_transmission_display())
+        if self.transmission == self.engine == 'all':
+            result = self.get_type_display()
+        return result
     type = models.CharField(u'модель', max_length=20, choices=CAR_TYPE)
-    engine = models.CharField(u'объем двигателя', max_length=4, choices=CAPACITY_CHOICES)
-    transmission = models.CharField(u'коробка передач', max_length=4, choices=TRANSMISSION_CHOICES)
+    engine = models.CharField(u'объем двигателя', max_length=4, choices=ENGINE_CAPACITY, default='all')
+    transmission = models.CharField(u'коробка передач', max_length=4, choices=TRANSMISSION_TYPE, default='all')
 
 
 class Spares(models.Model):
     class Meta:
         verbose_name_plural = u'запчасти'
         verbose_name = u'запчасть'
-        unique_together = ('name', 'car_type')
+        unique_together = ('name', 'car')
 
     def __unicode__(self):
         return '%s' % self.name
     name = models.CharField(u'название', max_length=100)
-    car_type = models.CharField(u'модель автомобиля', max_length=15, choices=CAR_TYPE)
-    price = models.DecimalField(u'цена', max_digits=12, decimal_places=2)
+    car = models.ForeignKey(Car, verbose_name=u'модель автомобиля', null=True)
+    price = models.DecimalField(u'цена', max_digits=9, decimal_places=2)
 
 
 class TechLiquids(models.Model):
@@ -71,7 +78,7 @@ class TechLiquids(models.Model):
     def __unicode__(self):
         return '%s' % self.name
     name = models.CharField(u'название', max_length=100)
-    car_type = models.CharField(u'модель автомобиля', max_length=15, choices=CAR_TYPE)
+    car = models.ForeignKey(Car, verbose_name=u'модель автомобиля', null=True)
     amount = models.DecimalField(u'кол-во, шт', max_digits=9, decimal_places=2, default=1.00)
     price = models.DecimalField(u'цена', max_digits=9, decimal_places=2)
 
@@ -83,11 +90,11 @@ class Service(models.Model):
 
     def __unicode__(self):
         return '%s' % self.name
-    type = models.CharField(u'тип', max_length=20, choices=SERVICE_TYPE)
+    type = models.CharField(u'тип', max_length=15, choices=SERVICE_TYPE)
     name = models.CharField(u'название', max_length=100)
-    car = models.CharField(u'модель автомобиля', max_length=15, choices=CAR_TYPE)
-    price = models.DecimalField(u'цена', max_digits=12, decimal_places=2)
-    price_cons = models.DecimalField(u'стоимость расходных материалов', max_digits=12, decimal_places=2, blank=True, null=True)
-    spares = models.ManyToManyField(Spares, verbose_name=u'запчасти', blank=True, null=True)
-    techliq = models.ManyToManyField(TechLiquids, verbose_name=u'тех.жидкости', blank=True, null=True)
+    car = models.ForeignKey(Car, verbose_name=u'модель автомобиля', null=True)
+    price = models.DecimalField(u'цена', max_digits=9, decimal_places=2)
+    price_cons = models.DecimalField(u'стоимость расходных материалов', max_digits=9, decimal_places=2, blank=True, null=True)
+    spares = models.ManyToManyField(Spares, verbose_name=u'запчасти', blank=True)
+    techliq = models.ManyToManyField(TechLiquids, verbose_name=u'тех.жидкости', blank=True)
 
