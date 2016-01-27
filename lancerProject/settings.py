0668bf391a16fd08958783088531a6ed13888ac6
Maksim Masterenko <mmasterenko@gmail.com>
2016-01-27 20:18:26 +0300

    sqlite db

--- lancerProject/settings.py
+++ lancerProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,16 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'lancerProject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -110,8 +101,9 @@ STATIC_URL = '/static/'
 # heroku settings #
 # # # # # # # # # #
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -130,4 +122,9 @@ try:
 except ImportError:
     pass
 
+
+try:
+    from settings_sqlite import *
+except ImportError:
+    pass
 
