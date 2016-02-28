fd2e3547ba3800a7fbab68edd0dab4cffbfce833
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-28 22:33:52 +0300

    Car model: engine field free of choices

--- lancerApp/models.py
+++ lancerApp/models.py
@@ -8,6 +8,7 @@ from .model_mixins import SlugNullField, SEOFieldsMixin
 upload_path = 'images/original'
 
 SERVICE_TYPE = (
+    ('to',        u'Тех.осмотр'),
     ('oil',       u'Замена жидкостей'),
     ('wheel',     u'Рулевое управление'),
     ('brake',     u'Тормозная система'),
@@ -15,7 +16,7 @@ SERVICE_TYPE = (
     ('engine',    u'Двигатель'),
     ('transmiss', u'Трансмиссия'),
     ('electro',   u'Электрика'),
-    ('other',   u'Другое'),
+    ('other',     u'Другое'),
 )
 
 
@@ -92,15 +93,10 @@ class Stuff(models.Model):
 
 
 class Car(models.Model):
-    class Meta:
-        verbose_name_plural = u'машины'
-        verbose_name = u'машина'
-        unique_together = ('type', 'engine', 'transmission')
-
     CAR_TYPE = (
         ('lancer9', 'Lancer 9'),
-        ('lancer10', 'Lancer 10'),
-        ('evo', 'Evolution 6-10'),
+        ('lancer10', 'Lancer X'),
+        ('evo', 'Evolution'),
         ('asx', 'Lancer ASX'),
         ('all', u'Все модели'),
     )
@@ -111,13 +107,14 @@ class Car(models.Model):
         ('all',  u'любая'),
     )
 
-    ENGINE_CAPACITY = (
-        ('1.4', u'1.4'),
-        ('1.6', u'1.6'),
-        ('1.8', u'1.8'),
-        ('2.0', u'2.0'),
-        ('all', u'любой'),
-    )
+    type = models.CharField(u'модель', max_length=20, choices=CAR_TYPE)
+    engine = models.CharField(u'двигатель', max_length=20, null=True, blank=True, unique=True)
+    transmission = models.CharField(u'коробка передач', max_length=4, choices=TRANSMISSION_TYPE, default='all')
+
+    class Meta:
+        verbose_name_plural = u'машины'
+        verbose_name = u'машина'
+        unique_together = ('type', 'engine', 'transmission')
 
     def __unicode__(self):
         result = '%s %s %s' % (self.get_type_display(), self.get_transmission_display(), self.get_engine_display())
@@ -132,10 +129,6 @@ class Car(models.Model):
     @property
     def name(self):
         return self.__unicode__()
-
-    type = models.CharField(u'модель', max_length=20, choices=CAR_TYPE)
-    engine = models.CharField(u'объем двигателя', max_length=4, choices=ENGINE_CAPACITY, default='all')
-    transmission = models.CharField(u'коробка передач', max_length=4, choices=TRANSMISSION_TYPE, default='all')
 
 
 class Spares(models.Model):
@@ -168,6 +161,16 @@ class TechLiquids(models.Model):
 
 
 class Service(models.Model):
+    type = models.CharField(u'тип', max_length=15, choices=SERVICE_TYPE)
+    name = models.CharField(u'название', max_length=100)
+    price = models.DecimalField(u'цена', max_digits=9, decimal_places=2)
+    price_cons = models.DecimalField(u'стоимость расходных материалов', max_digits=9, decimal_places=2, default=0)
+
+    car = models.ForeignKey(Car, verbose_name=u'модель автомобиля', null=True)
+
+    spares = models.ManyToManyField(Spares, verbose_name=u'запчасти', blank=True)
+    techliq = models.ManyToManyField(TechLiquids, verbose_name=u'тех.жидкости', blank=True)
+
     class Meta:
         verbose_name_plural = u'услуги'
         verbose_name = u'услуга'
@@ -193,12 +196,4 @@ class Service(models.Model):
         for typ, name in SERVICE_TYPE:
             if self.type == typ:
                 return name
-
-    type = models.CharField(u'тип', max_length=15, choices=SERVICE_TYPE)
-    name = models.CharField(u'название', max_length=100)
-    car = models.ForeignKey(Car, verbose_name=u'модель автомобиля', null=True)
-    price = models.DecimalField(u'цена', max_digits=9, decimal_places=2)
-    price_cons = models.DecimalField(u'стоимость расходных материалов', max_digits=9, decimal_places=2, default=0)
-    spares = models.ManyToManyField(Spares, verbose_name=u'запчасти', blank=True)
-    techliq = models.ManyToManyField(TechLiquids, verbose_name=u'тех.жидкости', blank=True)
 
