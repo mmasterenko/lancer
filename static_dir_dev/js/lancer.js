cdb4492fe11774cba0343c8116a73eee5c7c456d
Maksim Masterenko <mmasterenko@gmail.com>
2016-03-10 23:23:35 +0300

    html: choice car configuration

--- static_dir_dev/js/lancer.js
+++ static_dir_dev/js/lancer.js
@@ -1,4 +1,5 @@
-var all_cars, car_ids = [];
+var all_cars, car_ids = {};
+var service_urls = {};
 
 /*
  * {'lancer9': {'subtype':x, 'engine':x, 'transmission':x} }
@@ -44,8 +45,7 @@ function get_trans(cars, subtype, engine) {
 
 function analyze_subtypes(model, subtypes) {
 
-    console.log('analyze_subtypes: model:', model);
-    console.log('analyze_subtypes: subtypes:', subtypes);
+    console.log('analyze_subtypes: model, subtypes:', model, subtypes);
 
     if(_.size(subtypes) == 1) {
         // выбираем подвид
@@ -67,8 +67,7 @@ function analyze_subtypes(model, subtypes) {
 
 function analyze_engines(model, subtype, engines) {
 
-    console.log('analyze_engines: subtype:', subtype);
-    console.log('analyze_engines: engines:', engines);
+    console.log('analyze_engines: subtype, engines:', subtype, engines);
 
     if(_.size(engines) == 1){
         var engine = engines[0];
@@ -81,17 +80,16 @@ function analyze_engines(model, subtype, engines) {
         analyze_trans(model, subtype, engine, transmissions)
 
     } else {
-        // вывести меню двигателей
-        // + навешать на это меню колбэки
+
         set_engines(model, engines)
+
     }
 
 }
 
 function analyze_trans(model, subtype, engine, transmissions) {
 
-    console.log('analyze_trans: engine', engine);
-    console.log('analyze_trans: transmissions', transmissions);
+    console.log('analyze_trans: engine, transmissions:', engine, transmissions);
 
     if(_.size(transmissions) == 1){
         // это единственная машина. взять ее id.
@@ -103,15 +101,14 @@ function analyze_trans(model, subtype, engine, transmissions) {
 
         set_trans(model, subtype, engine, transmission);
     } else {
-        // вывести меню трансмиссий
-        // + навешать на это меню колбэки
+
+        set_menu(model, 'transmissions', transmissions);
+
     }
 }
 
 function set_menu(model, type, list) {
-    console.log('set_menu: model:', model);
-    console.log('set_menu: type:', type);
-    console.log('set_menu: list:', list);
+    console.log('set_menu: model, type, list:', model, type, list);
 
     var header = {
         'subtypes': 'Какая модель:',
@@ -119,15 +116,47 @@ function set_menu(model, type, list) {
         'transmissions': 'Какая у вас трансмиссия:'
     };
 
+    var trans = {
+        'auto': 'автоматическая',
+        'mech': 'механическая'
+    };
+
     var car_model = $('.car-model[data-car-model='+model+']');
 
     car_model.find('[data-choice-state=1]').find('.choice-header').text(header[type]);
     var choice_list = car_model.find('[data-choice-state=1]').find('.choice-list').html('');
 
-    var li, value, raw_value;
+    var li, value, raw_value, null_replace_trans;
+
+    list = _.map(list, function(x){if(x == 'null'){return null} return x});
+
+    var is_null_trans = (type == 'transmissions') && !_.every(list);
+
+    if(is_null_trans) {
+
+        if(_.size(list) == 2) {
+            var not_null_trans = _.max(list, Boolean);
+            if(not_null_trans == 'mech') null_replace_trans = 'auto';
+            if(not_null_trans == 'auto') null_replace_trans = 'mech';
+        }
+
+        // удаляем null трансмиссию из списка
+        if(_.size(list) == 3) {
+            list = _.filter(list);
+        }
+
+    }
+
     for(var i in list){
+
         raw_value = list[i];
         value = list[i] || 'другое';
+
+        if(type == 'transmissions') {
+            if(!raw_value) raw_value = null_replace_trans;
+            value = trans[raw_value];
+        }
+
         li = $('<li data-capacity="'+value+'"><a href="javascript:void(0)">'+value+'</a></li>');
         li.on('click', (function (model, type, raw_value) {
             return function (event) {
@@ -141,27 +170,46 @@ function set_menu(model, type, list) {
 
 function click_menu(model, type, raw_value) {
 
-    console.log('click_menu:', model, type, raw_value);
-    console.log(current_choice);
+    console.log('click_menu: model, type, raw_value:', model, type, raw_value);
+    console.log('click_menu: current_choice:', current_choice);
 
-    var subtype, engine;
+    var subtype, engine, transmission, obj;
 
     if(type == 'subtypes') {
         subtype = raw_value;
+
+        obj = current_choice[model] || {};
+        obj['subtype'] = subtype;
+        current_choice[model] = obj;
+
         var engines = get_engines(all_cars[model], subtype);
+
         analyze_engines(model, subtype, engines);
     }
 
     if(type == 'engines') {
         subtype = current_choice[model]['subtype'];
         engine = raw_value;
+
+        obj = current_choice[model] || {};
+        obj['engine'] = engine;
+        current_choice[model] = obj;
+
         var transmissions = get_trans(all_cars[model], subtype, engine);
 
         analyze_trans(model, subtype, engine, transmissions);
     }
 
     if(type == 'transmissions') {
+        subtype = current_choice[model]['subtype'];
+        engine = current_choice[model]['engine'];
+        transmission = raw_value;
 
+        obj = current_choice[model] || {};
+        obj['transmission'] = transmission;
+        current_choice[model] = obj;
+
+        set_trans(model, subtype, engine, transmission);
     }
 
 }
@@ -180,18 +228,44 @@ function set_subtypes(model, subtypes) {
 
 }
 
+// выбирает id машин имея полную конфигурацию
 function set_trans(model, subtype, engine, transmission) {
 
-    var i, car, id;
+    console.log('set_trans: current_choice:', current_choice);
+
+    var car, id;
     var cars = all_cars[model];
-    for(i in cars) {
+
+    for(var i in cars) {
         car = cars[i];
-        if(car['subtype'] == subtype && car['engine'] == engine && car['transmission'] == transmission) {
-            id = car['id'];
-            car_ids.push(id);
-            // добавить логику для mech auto null
+        id = car['id'];
+
+        if(car['subtype'] == subtype && car['engine'] == engine) {
+
+            if(car['transmission'] == transmission || !car['transmission']){
+
+                var arr = car_ids[model] || [];
+                arr.push(id);
+                car_ids[model] = arr;
+            }
         }
     }
+
+    var car_model = $('.car-model[data-car-model='+model+']');
+
+    // делаем активным меню выбора услуг
+    var choiceContainer = car_model.find('.choice-container');
+    choiceContainer.children().each(function(){$(this).removeClass('active')});
+    choiceContainer.find('[data-choice-state=2]').addClass('active');
+
+    var ids = car_ids[model];
+
+    var url = 'cars=' + ids.toString();
+    choiceContainer.find('[data-choice-state=2] .sub-list a').each(function($index){
+        var basic_url = service_urls[model][$index];
+        $(this).attr('href', basic_url + '?' + url);
+    });
+
 }
 
 
@@ -202,18 +276,13 @@ $(document).ready(function(){
     });
 
     /*
-     * {'lancer9': ['mechanic', '1.3'] }
-     */
-    var choosedEngine = {};
-
-    /*
-     * urls:
+     * запоминаем оригинальные URL услуг
+     *
+     * service_urls:
      * {'lancer9':  ['/repair', '/tech', ]
      *  'lancer10': ['/repair', '/tech', ]
      * }
      * */
-    var service_urls = {};
-
     $('[data-car-model]').each(function(){
         var model = $(this).data('carModel');
         var urls = [];
@@ -239,39 +308,16 @@ $(document).ready(function(){
     $('.car-model .choice .back a').on('click', function(){
         var choiceContainer = $(this).parents('.choice-container');
         choiceContainer.children().each(function(){$(this).removeClass('active')});
-        $(this).parents('.choice').prev().addClass('active');
+        choiceContainer.find('[data-choice-state=0]').addClass('active');
+        var model = choiceContainer.parent('.car-model').data('carModel');
+        car_ids[model] = [];
     });
-
-    // ссылка: коробка передач: механическая и автоматическая
-    $('.car-model .choice .transmission>a').on('click', function(){
-        $(this).next().toggleClass('active');
-        var e = $(this).parent().siblings().find('.sub-list').removeClass('active');
-    });
-
-    // ссылка: объем двигателя
-    $('.car-model .choice[data-choice-state=1] .sub-list a').on('click', function(){
-        var choiceContainer = $(this).parents('.choice-container');
-        choiceContainer.children().each(function(){$(this).removeClass('active')});
-        choiceContainer.find('[data-choice-state=2]').addClass('active');
-
-        var model = $(this).parents('[data-car-model]').data('carModel');
-        var transType = $(this).parents('[data-trans-type]').data('transType');
-        var capacity = $(this).parents('[data-capacity]').data('capacity');
-        choosedEngine[model] = [transType, capacity];
-        var url = '/' + model + '/' + transType + '/' + capacity;
-        choiceContainer.find('[data-choice-state=2] .sub-list a').each(function($index){
-            var basic_url = service_urls[model][$index];
-            $(this).attr('href', basic_url + url);
-        });
-
-    });
-
 
     /*
        всплывающее окошко: "перезвонить мне"
      */
     $('#header .callme > a').on('click', function(){
-        $('#callme').toggle('fade', 400);
+        $('#callme').toggle(400);
     });
 
     $('#callme div a').on('click', function(){
@@ -279,7 +325,7 @@ $(document).ready(function(){
     });
 
     $('#callme a.close').on('click', function(){
-        $('#callme').toggle('fade', 500);
+        $('#callme').toggle(500);
     });
 
 });
