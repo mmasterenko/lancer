6afd20ed3249699f19bc22c7fd0a341ba59d6ea0
Maksim Masterenko <mmasterenko@gmail.com>
2016-03-19 14:13:39 +0300

    import_csv fix

--- import_csv.py
+++ import_csv.py
@@ -7,6 +7,8 @@ from lancerApp.models import Service
 
 
 def import_service(car_id, stype, fd):
+    total = 0
+    new = 0
     reader = csv.reader(fd)
     skip_lines = 2
     for row in reader:
@@ -20,12 +22,17 @@ def import_service(car_id, stype, fd):
         if not price:
             continue
         price_cons = row[4] if row[4] else 0
-        Service.objects.create(car_id=car_id, type=stype, name=name, price=price, price_cons=price_cons)
+        obj, created = Service.objects.get_or_create(car_id=car_id, type=stype, name=name, price=price, price_cons=price_cons)
+        total += 1
+        if created:
+            new += 1
+    print 'Total: %s' % total
+    print 'New: %s' % new
 
 
 if __name__ == '__main__':
-    stype = 'oil'
+    stype = 'wheel'
     car_id = 32
-    fd = open(os.path.join(settings.BASE_DIR, 'devfiles/Lancer_x_techliq.csv'))
+    fd = open(os.path.join(settings.BASE_DIR, 'devfiles/Lancer_x_wheel.csv'))
 
     import_service(car_id, stype, fd)
